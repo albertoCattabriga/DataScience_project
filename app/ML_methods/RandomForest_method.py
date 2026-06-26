5ec7de9fb8c1732c23c438a0b75579d07497a69b
ALBERTO CATTABRIGA <293361@studenti.unimore.it>
2026-06-26 13:08:19 +0200

    change format

--- app/ML_methods/RandomForest_method.py
+++ app/ML_methods/RandomForest_method.py
@@ -17,7 +17,7 @@ def RandomForest_prediction(X_train, X_test, Y_train, Y_test, gender, age:int, s
 
     # Find the best number of estimator and the best max depth.
     set_par = {'n_estimators' : list(range(50, 250, 50)),
-                     'max_depth' : list(range(1, 10))}
+                  'max_depth' : list(range(1, 10))}
 
     search_par = GridSearchCV(RandomForestClassifier(), set_par, cv=5, n_jobs=-1,
                                scoring='f1_weighted')
